bc7022f11ddbd7331d1473615081dc680c6081ba
Peter Eisentraut <peisentraut@meetme.com>
2013-08-12 15:50:21 -0400

    [COOK-3441] Make user password optional

--- libraries/resource_database_user.rb
+++ libraries/resource_database_user.rb
@@ -56,8 +56,7 @@ class Chef
         set_or_return(
           :password,
           arg,
-          :kind_of => String,
-          :required => true
+          :kind_of => String
         )
       end
 
