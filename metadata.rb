d02fb5490c6f1d964e68930b23306c6eb656b544
Sean OMeara <sean@chef.io>
2014-12-13 12:53:11 -0500

    locking mysql dependencies down in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -12,11 +12,11 @@ recipe 'database::ebs_volume', 'Sets up an EBS volume in EC2 for the database'
 recipe 'database::master', 'Creates application specific user and database'
 recipe 'database::snapshot', 'Locks tables and freezes XFS filesystem for replication, assumes EC2 + EBS'
 
-depends 'mysql', '>= 5.0.0'
+depends 'mysql', '~> 5.0'
 depends 'postgresql', '>= 1.0.0'
 depends 'aws'
 depends 'xfs'
-depends 'mysql-chef_gem'
+depends 'mysql-chef_gem', '~> 0.0'
 
 %w(debian ubuntu centos suse fedora redhat scientific amazon).each do |os|
   supports os
