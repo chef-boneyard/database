285a34cd60790792caeccc8ab85117e6be75b39c
Lucas Mariani <lmariani@lojack.com.ar>
2015-02-10 23:28:30 -0300

    mysql-gem_chef restricted version

--- metadata.rb
+++ metadata.rb
@@ -16,7 +16,7 @@ depends 'mysql', '>= 5.0.0'
 depends 'postgresql', '>= 1.0.0'
 depends 'aws'
 depends 'xfs'
-depends 'mysql-chef_gem'
+depends 'mysql-chef_gem', '<= 0.0.5'
 
 %w(debian ubuntu centos suse fedora redhat scientific amazon).each do |os|
   supports os
