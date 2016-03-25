6bd7e89f34abe903ff956e7b5fdf82b4e4ce737b
Ronald Doorn <rdoorn@schubergphilis.com>
2016-03-25 10:22:23 +0100

    Sqlite provider added + fixes in sql_server_user provider
    fix unrelated mysql error on grant
    
    rubo cop fix

---
+++ libraries/resource_sqlite_database.rb
@@ -0,0 +1,40 @@
+#
+# Author:: Ronald Doorn (<rdoorn@schubergphilis.com>)
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require File.join(File.dirname(__FILE__), 'resource_database')
+require File.join(File.dirname(__FILE__), 'provider_database_sqlite')
+
+class Chef
+  class Resource
+    class SqliteDatabase < Chef::Resource::Database
+      def initialize(name, run_context = nil)
+        super
+        @resource_name = :sqlite_database
+        @provider = Chef::Provider::Database::Sqlite
+      end
+
+      def sql(arg = nil, &block)
+        arg ||= block
+        set_or_return(
+          :sql,
+          arg,
+          kind_of: [String, Proc, Array]
+        )
+      end
+    end
+  end
+end
