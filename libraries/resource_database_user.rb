85f42b80cf471c789603bea8d4b7931f04b761e8
Tim Smith <tsmith84@gmail.com>
2016-04-08 11:04:27 -0700

    Add password back to the high level user

--- libraries/resource_database_user.rb
+++ libraries/resource_database_user.rb
@@ -61,6 +61,14 @@ class Chef
         )
       end
 
+      def password(arg = nil)
+        set_or_return(
+          :password,
+          arg,
+          kind_of: String
+        )
+      end
+
       def table(arg = nil)
         set_or_return(
           :table,
