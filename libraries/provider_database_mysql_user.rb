d174ed99035764b07ad4f22b7604c03940e8ad52
Alexander Zubkov <info@zubkov.info>
2016-09-16 17:57:07 +0300

    Fix again undefined method `empty?' for Mysql2::Result

--- libraries/provider_database_mysql_user.rb
+++ libraries/provider_database_mysql_user.rb
@@ -116,7 +116,7 @@ class Chef
             test_sql += " AND Db='#{new_resource.database_name}'" if new_resource.database_name
             test_sql_results = test_client.query test_sql
 
-            incorrect_privs = true if test_sql_results.empty?
+            incorrect_privs = true if test_sql_results.size == 0
             # These should all be 'Y'
             test_sql_results.each do |r|
               desired_privs.each do |p|
@@ -336,7 +336,7 @@ class Chef
                           "AND authentication_string=PASSWORD('#{new_resource.password}')"
                         end
           end
-          !test_client.query(test_sql).empty?
+          test_client.query(test_sql).size > 0
         end
 
         def update_user_password
@@ -367,7 +367,7 @@ class Chef
         end
 
         def database_has_password_column(client)
-          !client.query('SHOW COLUMNS FROM mysql.user WHERE Field="Password"').empty?
+          client.query('SHOW COLUMNS FROM mysql.user WHERE Field="Password"').size > 0
         end
       end
     end
